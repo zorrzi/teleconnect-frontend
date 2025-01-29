d3cf6fe35465da63144d41586228cd3a91c1ac99
Henrique-Albuquerque2 <henriqueaa1@al.insper.edu.br>
2025-01-29 16:42:58 -0300

    Base Produtos

--- teleconnect-frontend/src/pages/user/produtos/pessoa/nested/Pre_Pago/Pre_Pago.tsx
+++ teleconnect-frontend/src/pages/user/produtos/pessoa/nested/Pre_Pago/Pre_Pago.tsx
@@ -1,21 +1,50 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
+import { listPackages, Package } from "../../../api/getPackages";
+import { PackageCard } from "../../../components/PackageCard";
 
 export const PrePagoPage = () => {
+    const [packages, setPackages] = useState<Package[]>([]);
+
+    useEffect(() => {
+        async function fetchPackages() {
+            const { data } = await listPackages(); // 🔹 Extraindo "data" da resposta
+            const filteredPackages = data.filter(pkg => pkg.mobile_service === "Pré-pago");
+            setPackages(filteredPackages);
+        }
+
+        fetchPackages();
+    }, []);
+
     return (
         <Container>
-            <Title>Planos Pré Pago</Title>
+            <Title>Escolha seu plano Pré-Pago</Title>
+            <PackagesGrid>
+                {packages.map(pack => (
+                    <PackageCard key={pack._id} pack={pack} />
+                ))}
+            </PackagesGrid>
         </Container>
     );
 };
 
 /* ======= Styled Components ======= */
+
 const Container = styled.div`
-    padding: 20px;
+    padding: 40px;
+    text-align: center;
 `;
 
 const Title = styled.h2`
-    text-align: center;
-    font-size: 2rem;
+    font-size: 1.8rem;
+    font-weight: bold;
+    color: #003B5C;
     margin-bottom: 20px;
-    color: #333;
+`;
+
+const PackagesGrid = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    gap: 20px;
+    justify-content: center;
 `;
